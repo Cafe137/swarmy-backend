d87bc049213bb76810c0870c37e6d802e8961d15
Cafe137 <xcafe137@gmail.com>
2025-01-26 22:46:57 +0100

    fix: add missing module import

--- src/app/crypto-payment/crypto-payment.module.ts
+++ src/app/crypto-payment/crypto-payment.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EmailModule } from '../email/email.module';
+import { PlanModule } from '../plan/plan.module';
 import { CryptoPaymentController } from './crypto-payment.controller';
 import { CryptoPaymentScheduledService } from './crypto-payment.scheduled.service';
 import { CryptoPaymentService } from './crypto-payment.service';
 
 @Module({
-  imports: [EmailModule],
+  imports: [EmailModule, PlanModule],
   controllers: [CryptoPaymentController],
   providers: [CryptoPaymentService, CryptoPaymentScheduledService],
   exports: [],
